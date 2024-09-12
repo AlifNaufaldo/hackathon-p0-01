575306440b1e94f64e63f92821f9bb0e379d9d7b
Prima <widianiprima@gmail.com>
2024-09-12 17:19:49 +0700

    Update

--- list.js
+++ list.js
@@ -13,43 +13,46 @@ let years = ['2010', '2016', '2022', '2021', '2019', '2024']
 let price = [1000, 5000, 13455, 34531, 423234, 13467]
 //buat push
 let data = [
-    {"brandname": "Toyota", "name": "Avanza", "cc": 1300, "years": 2010, "price": 1000},
-    {"brandname": "Honda", "name": "Brio", "cc": 1200, "years": 2016, "price": 5000},
-    {"brandname": "Daihatsu", "name": "Xenie", "cc": 1000, "years": 2022, "price": 13455},
-    {"brandname": "Nissan", "name": "Skyline", "cc": 2600, "years": 2021, "price": 34531},
-    {"brandname": "Mitsubishi", "name": "L300", "cc": 2268, "years": 2019, "price": 423234},
-    {"brandname": "Hino", "name": "Hino 700", "cc": 12913, "years": 2024, "price": 13467},
+    {"brandname": "Toyota", "name": "Avanza", "cc": 1300, "years": 2010, "price": 1000, "id": 1},
+    {"brandname": "Honda", "name": "Brio", "cc": 1200, "years": 2016, "price": 5000, "id": 2},
+    {"brandname": "Daihatsu", "name": "Xenie", "cc": 1000, "years": 2022, "price": 13455, "id": 3},
+    {"brandname": "Nissan", "name": "Skyline", "cc": 2600, "years": 2021, "price": 34531, "id": 4},
+    {"brandname": "Mitsubishi", "name": "L300", "cc": 2268, "years": 2019, "price": 423234, "id": 5},
+    {"brandname": "Hino", "name": "Hino 700", "cc": 12913, "years": 2024, "price": 13467, "id": 6},
     
 ]
 
 
 
-function createDataBaru(input, data){
-    
+function createDataBaru(){
 
     let brandName = document.getElementById('brandname').value;
-    let model = document.getElementById('model').value;
+    let model = document.getElementById('name').value;
     let CC = document.getElementById('CC').value;
     let years = document.getElementById('years').value;
     let price = document.getElementById('price').value;
 
-    if (input.brandName && input.model && input.CC && input.years && input.price){
-        let databaru = {
-            brandname: input.brandName,
-            name: input.model,
-            cc: input.CC,
-            years: input.years,
-            price: input.price
+    if (brandName && model && cc && years && price){
+        let idData = data.length === 0 ? 1 : data[data.length - 1] + 1;
+
+        let dataBaru = {
+            brandname: brandname,
+            name: model,
+            CC: CC,
+            years: years,
+            price: price,
+            id: idData
         }
 
-        data.push(databaru)
+        data.push(dataBaru);
+        
+        console.log(data);
+        // document.getElementById('formulir').reset();
+        // render(data);
+    } else {
+        alert(`DATA HARUS DIISI! NIAT JUALAN GAK SIH?`);
     }
 
-    else{
-        return `DATA HARUS DIISI! NIAT JUALAN GAK SIH?`
-    }
-
-    return data
 }
 
-console.log(createDataBaru(input, data));
+// createDataBaru({"brandname": "Hino", "name": "Hino 700", "cc": 12913, "years": 2024, "price": 13467})
