6ea99b0a551297af56e1233c5617e0983fa33eee
Shaqila Thurfa <shaqilagunawan20@gmail.com>
2024-09-12 16:05:24 +0700

    create

--- list.js
+++ list.js
@@ -19,11 +19,37 @@ let data = [
     {"brandname": "Nissan", "name": "Skyline", "cc": 2600, "years": 2021, "price": 34531},
     {"brandname": "Mitsubishi", "name": "L300", "cc": 2268, "years": 2019, "price": 423234},
     {"brandname": "Hino", "name": "Hino 700", "cc": 12913, "years": 2024, "price": 13467},
+    
 ]
 
-// function cariHarga(input, data){
-    
-    
-// }
 
-// console.log(cariHarga(1000, data));
+
+function createDataBaru(input, data){
+    
+
+    let brandName = document.getElementById('brandname').value;
+    let model = document.getElementById('model').value;
+    let CC = document.getElementById('CC').value;
+    let years = document.getElementById('years').value;
+    let price = document.getElementById('price').value;
+
+    if (input.brandName && input.model && input.CC && input.years && input.price){
+        let databaru = {
+            brandname: input.brandName,
+            name: input.model,
+            cc: input.CC,
+            years: input.years,
+            price: input.price
+        }
+
+        data.push(databaru)
+    }
+
+    else{
+        return `DATA HARUS DIISI! NIAT JUALAN GAK SIH?`
+    }
+
+    return data
+}
+
+console.log(createDataBaru(input, data));
