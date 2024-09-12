c5a0c8f6a5b0cfbc80187020c7529b18c86cd0ab
AlifNaufaldo <muhammadalifnaufaldo@gmail.com>
2024-09-12 14:30:58 +0700

    list data

---
+++ list.js
@@ -0,0 +1,13 @@
+let list = {
+    brandname: '',
+    name: '',
+    CC: '',
+    years: '',
+    price: 0,
+}
+
+let brandname = ['Toyota', 'Honda', 'Daihatsu', 'Nissan', 'Mitsubishi', 'Hino']
+let name = ['Avanza', 'Brio', 'Xenia', 'Skyline', 'L300', 'Hino 700']
+let cc = ['1300', '1200','1000', '2600', '2268', '12913']
+let years = ['2010', '2016', '2022', '2021', '2019', '2024']
+let price = [1000, 5000, 13455, 34531, 423234, 13467]
